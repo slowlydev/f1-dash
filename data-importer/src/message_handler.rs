c74107f7ddf864dee3283225534641577c866433
Slowlydev <slowly.developers@gmail.com>
2023-05-19 15:51:35 +0200

    feat(data-importer): implemented last lap time timing

--- data-importer/src/message_handler.rs
+++ data-importer/src/message_handler.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
+
 use scylla::Session;
 use serde_json::Value;
 use uuid::Uuid;
 
 use crate::{
-    live_timing_models::{RaceControlMessages, WeatherMessage},
-    models::{RaceControlMessage, WeatherData},
+    live_timing_models::{Driver, RaceControlMessages, TimingDataMessage, WeatherMessage},
+    models::{LastLapTime, RaceControlMessage, WeatherData},
     utils::{parse_float, parse_int},
     SocketData,
 };
@@ -44,14 +46,14 @@ pub async fn handle(socket_data: SocketData, session: &Session) {
 		};
 
     match &parsed_cat[..] {
-        "WeatherData" => handle_weather_data(msg, parsed_time, session).await,
+        "WeatherData" => handle_weather_data(msg, session, parsed_time).await,
         "RaceControlMessages" => handle_race_control_messages(msg, session).await,
-        // "TimingData" => extract_timing_data(message, datetime),
+        "TimingData" => handle_timing_data(msg, session, parsed_time).await,
         _ => (),
     };
 }
 
-async fn handle_weather_data(msg: &Value, time: &str, session: &Session) {
+async fn handle_weather_data(msg: &Value, session: &Session, time: &str) {
     let Ok(parsed_msg) = serde_json::from_value::<WeatherMessage>(msg.clone()) else {
     	println!("Failed to parse msg");
     	return;
@@ -113,12 +115,50 @@ async fn handle_race_control_messages(msg: &Value, session: &Session) {
         .expect("Failed to insert");
 }
 
-// async fn handle_timing_data(message: Value, session: &Session) {
-//     let Some(lines) = message["Lines"].as_object() else {
-//         println!("Failed to get lines object");
-//         return;
-//     };
+async fn handle_timing_data(msg: &Value, session: &Session, time: &str) {
+    let Ok(parsed_msg) = serde_json::from_value::<TimingDataMessage>(msg.clone()) else {
+		println!("Failed to parse msg");
+		return;
+	};
 
-//     println!("{:?}", lines);
-//     // handle_last_lap_time(lines);
-// }
+    let lines: HashMap<String, Driver> = parsed_msg.Lines;
+
+    // println!("{:?}", lines);
+
+    // last lap time
+
+    let prepared_lap_time_query = session
+        .prepare("INSERT INTO f1_dash.last_lap_time (id, lap_time, personal_best, time) VALUES (?, ?, ?, ?)")
+        .await
+        .expect("Failed to prepare last lap query");
+
+    for entity in handle_last_lap_time(lines, time) {
+        println!("{:?}", entity);
+
+        session
+            .execute(&prepared_lap_time_query, entity)
+            .await
+            .expect("Failed to insert");
+    }
+}
+
+fn handle_last_lap_time(lines: HashMap<String, Driver>, time: &str) -> Vec<LastLapTime> {
+    let mut results: Vec<LastLapTime> = Vec::new();
+
+    lines.iter().for_each(|(_driver_nr, driver)| {
+        let uuid = Uuid::new_v4();
+
+        let Some(lap_time) = &driver.LastLapTime else {
+            return;
+        };
+
+        results.push(LastLapTime {
+            id: uuid,
+            lap_time: lap_time.Value.to_string(),
+            personal_best: lap_time.PersonalFastest,
+            time: time.to_string(),
+        });
+    });
+
+    results
+}
