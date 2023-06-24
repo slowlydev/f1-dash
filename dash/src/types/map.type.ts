3e7b91ae99ac02f111a9081f82b163433b53a2b6
Slowlydev <slowly.developers@gmail.com>
2023-06-24 19:02:05 +0200

    feat(dash-map-api): add route to get map data

---
+++ dash/src/types/map.type.ts
@@ -0,0 +1,44 @@
+export type MapType = {
+  corners: Corner[];
+  marshalLights: Corner[];
+  marshalSectors: Corner[];
+  candidateLap: CandidateLap;
+  circuitKey: number;
+  circuitName: string;
+  countryIocCode: string;
+  countryKey: number;
+  countryName: string;
+  location: string;
+  meetingKey: string;
+  meetingName: string;
+  meetingOfficialName: string;
+  raceDate: string;
+  rotation: number;
+  round: number;
+  trackPositionTime: number[];
+  x: number[];
+  y: number[];
+  year: number;
+};
+
+type CandidateLap = {
+  driverNumber: string;
+  lapNumber: number;
+  lapStartDate: string;
+  lapStartSessionTime: number;
+  lapTime: number;
+  session: string;
+  sessionStartTime: number;
+};
+
+type Corner = {
+  angle: number;
+  length: number;
+  number: number;
+  trackPosition: TrackPosition;
+};
+
+type TrackPosition = {
+  x: number;
+  y: number;
+};
