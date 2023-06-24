18fa98f417273ec78fa0b61fec857ceed6666045
Slowlydev <slowly.developers@gmail.com>
2023-06-24 21:54:30 +0200

    fix(dash-enabled-features): check if windows is defined

--- dash/src/lib/getEnabledFeatures.ts
+++ dash/src/lib/getEnabledFeatures.ts
@@ -1,3 +1,4 @@
 export const getEnabledFeatures = (): string[] => {
+  if (typeof window === "undefined") return [];
   return JSON.parse(localStorage.getItem("experimentalFeatures") ?? "[]") ?? [];
 };
