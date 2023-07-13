82d01c177d24c96caacc653723aa4fce753b5da4
Slowlydev <slowly.developers@gmail.com>
2023-07-13 20:33:44 +0200

    feat(dash): add audio progress bar component

---
+++ dash/src/components/AudioProgress.tsx
@@ -0,0 +1,20 @@
+import { motion } from "framer-motion";
+
+type Props = {
+  duration: number;
+  progress: number;
+};
+
+export default function AudioProgress({ duration, progress }: Props) {
+  const percent = progress / duration;
+
+  return (
+    <div className="h-2 w-60 overflow-hidden rounded-xl bg-white bg-opacity-50">
+      <motion.div
+        className="h-2 bg-white"
+        style={{ width: `${percent * 100}%` }}
+        layout
+      />
+    </div>
+  );
+}
