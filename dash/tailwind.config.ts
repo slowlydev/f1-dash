513bbdd3b9edbba2dfc5227df3a995757a094103
Slowlydev <slowly.developers@gmail.com>
2023-07-13 21:26:34 +0200

    feat(dash): extend tailwind theme with even bigger screen size

--- dash/tailwind.config.ts
+++ dash/tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1800px",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
