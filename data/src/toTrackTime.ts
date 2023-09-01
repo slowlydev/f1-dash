57138dd665e819c97fdd471bd803be11e17ec28a
Slowlydev <slowly.developers@gmail.com>
2023-09-01 20:40:52 +0200

    fix(data): toTrackTime uses utc for everything now and added docs

--- data/src/toTrackTime.ts
+++ data/src/toTrackTime.ts
@@ -1,12 +1,27 @@
+/**
+ * Welcome to the most scuffed time/date conversion.
+ *
+ * We assume the offset has this patter: HH:mm:ss
+ *
+ * We also assume that the utc date provided does not have a Z to indicate utc, because why not F1
+ *
+ * We extract the h m s from our string and parse to ints/numbers
+ * We individually update our original date with hours, minutes and seconds.
+ * *
+ * @param utc
+ * @param offset
+ * @returns ISO-8601 string
+ */
 export const toTrackTime = (utc: string, offset: string) => {
-	const utcDate = new Date(utc);
+	const date = new Date(`${utc}.000Z`);
 
 	const [hours, minutes, seconds]: (number | undefined)[] = offset.split(":").map((unit) => parseInt(unit));
 
-	const trackTime = new Date();
-	trackTime.setHours(utcDate.getHours() + hours);
-	trackTime.setMinutes(utcDate.getMinutes() + minutes);
-	trackTime.setSeconds(utcDate.getSeconds() + seconds);
+	console.log({ hours, minutes, seconds });
 
-	return trackTime.toISOString();
+	date.setUTCHours(date.getUTCHours() + hours);
+	date.setUTCMinutes(date.getUTCMinutes() + minutes);
+	date.setUTCSeconds(date.getUTCSeconds() + seconds);
+
+	return date.toISOString();
 };
