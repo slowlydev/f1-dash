02856a6ed2c15694be2a87b2a5efb5ba5a2c1d8f
Slowlydev <slowly.developers@gmail.com>
2023-10-29 15:38:50 +0100

    refactor(dash): better handeling for delay input

--- dash/src/components/DelayInput.tsx
+++ dash/src/components/DelayInput.tsx
@@ -1,21 +1,32 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 type Props = {
   setDebouncedDelay: (delay: number) => void;
 };
 
 export default function DelayInput({ setDebouncedDelay }: Props) {
-  const [delay, setDelay] = useState("0");
+  const [delay, setDelay] = useState("");
+
+  const updateDebounced = () => {
+    const nextDelay = parseInt(delay ?? "0");
+    if (nextDelay < 0) return;
+    setDebouncedDelay(nextDelay);
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    updateDebounced();
+  };
 
   return (
-    <div className="sm:mr-2">
+    <form onSubmit={handleSubmit}>
       <input
         value={delay}
         onChange={(e) => setDelay(e.target.value)}
-        onBlur={() => setDebouncedDelay(parseInt(delay))}
+        onBlur={() => updateDebounced()}
         placeholder="delay"
         className="block w-20 rounded-lg border-[1px] border-gray-500 bg-zinc-900 py-1 text-center font-mono"
       />
-    </div>
+    </form>
   );
 }
