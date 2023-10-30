eb1fd9331c141f75be819bc490bc3957534197a9
Slowlydev <slowly.developers@gmail.com>
2023-10-30 22:52:29 +0100

    feat(dash): add more links to navbar

--- dash/src/components/Navbar.tsx
+++ dash/src/components/Navbar.tsx
@@ -9,23 +9,29 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <div className="flex h-8 flex-row items-center gap-2 truncate rounded-md bg-zinc-800">
-      <Link href="/" className="h-full">
-        <NavItem name="Dashboard" active={pathname === "/"} />
+    <div className="flex h-8 w-min flex-row items-center gap-2 truncate rounded-md bg-zinc-800">
+      <Link href="/home" className="h-full">
+        <NavItem name="Home" active={pathname === "/home"} />
       </Link>
 
       <Link href="/archive" className="h-full">
         <NavItem name="Archive" active={pathname === "/archive"} />
       </Link>
 
+      <Link href="/help" className="h-full">
+        <NavItem name="Help" active={pathname === "/help"} />
+      </Link>
+
+      <div className="h-full w-[2px] bg-gray-500" />
+
+      <Link href="/" className="h-full">
+        <NavItem name="Dashboard" active={pathname === "/"} />
+      </Link>
+
       {/* <NavItem name="H2H View" active={false} /> */}
       {/* <NavItem name="Team View" active={false} /> */}
       {/* <NavItem name="Graph View" active={false} /> */}
       {/* <NavItem name="Settings" active={false} /> */}
-
-      <Link href="/help" className="h-full">
-        <NavItem name="Help?" active={pathname === "/help"} />
-      </Link>
     </div>
   );
 }
