6938dc850c1d42f600b44442a121cc76baba0d6a
Slowlydev <slowly.developers@gmail.com>
2023-10-31 19:08:32 +0100

    feat(dash): finsihed home page

--- dash/src/app/(nav)/home/page.tsx
+++ dash/src/app/(nav)/home/page.tsx
@@ -25,6 +25,8 @@ import Button from "@/components/Button";
 
 import { NextMeeting } from "@/types/nextMeeting.type";
 import HomeRoadmapItem from "../../../components/HomeRoadmapItem";
+import HomeFeatureCard from "../../../components/HomeFeatureCard";
+import DriverDRS from "../../../components/DriverDRS";
 
 const getNextMeeting = async (): Promise<NextMeeting> => {
 	const req = await fetch(`${env.NEXT_PUBLIC_SERVER_URL}/api/next-meeting`);
@@ -61,54 +63,46 @@ export default async function Page() {
 				<h2 className="mb-4 text-3xl font-bold">Core Features</h2>
 
 				<div className="grid grid-cols-1 gap-2 sm:grid-cols-2 xl:grid-cols-4">
-					<div className={featureClass}>
-						<p className="text-lg leading-none">Tires & Pitstops</p>
-						<p className="leading-none text-gray-400">
-							Discover which tires the drivers are using and how old they are. As well as their numbers of pitstops.
-						</p>
-						<div className="flex gap-2 rounded-lg bg-zinc-900 p-4">
-							<div className="flex space-x-[-1rem] ">
-								<Image src={softTireIcon} alt="soft" className="h-8 w-8" />
-								<Image src={mediumTireIcon} alt="medium" className="h-8 w-8" />
-								<Image src={hardTireIcon} alt="hard" className="h-8 w-8" />
-								<Image src={intermediateTireIcon} alt="intermediates" className="h-8 w-8" />
-								<Image src={wetTireIcon} alt="wets" className="h-8 w-8" />
-							</div>
-
-							<DriverTire stints={exampleDriver.stints} />
+					<HomeFeatureCard
+						title="Tires & Pitstops"
+						description="Discover which tires the drivers are using and how old they are. As well as their numbers of pitstops."
+					>
+						<div className="flex space-x-[-1rem] ">
+							<Image src={softTireIcon} alt="soft" className="h-8 w-8" />
+							<Image src={mediumTireIcon} alt="medium" className="h-8 w-8" />
+							<Image src={hardTireIcon} alt="hard" className="h-8 w-8" />
+							<Image src={intermediateTireIcon} alt="intermediates" className="h-8 w-8" />
+							<Image src={wetTireIcon} alt="wets" className="h-8 w-8" />
 						</div>
-					</div>
 
-					<div className={featureClass}>
-						<p className="text-lg font-medium leading-none">Sectores</p>
-						<p className="leading-none text-gray-400">
-							See Sectore Times and even Minisector data, to figure out whos on a potential personal best or even
-							fastest lap.
-						</p>
-						<div className="rounded-lg bg-zinc-900 p-4">
-							<DriverMiniSectors driverDisplayName="test" sectors={exampleDriver.sectors} />
-						</div>
-					</div>
+						<DriverTire stints={exampleDriver.stints} />
+					</HomeFeatureCard>
 
-					<div className={featureClass}>
-						<p className="text-lg font-medium leading-none">Track Map</p>
-						<p className="leading-none text-gray-400">
-							See the postion of the drivers in realtime. Where and if drivers crashed and their status.
-						</p>
-						<div className="rounded-lg bg-zinc-900">
-							<Image src={mapFeature} alt="" className="rounded-lg" />
-						</div>
-					</div>
+					<HomeFeatureCard
+						title="Sector Times"
+						description="See Sectore Times and even Minisector data, to figure out whos on a potential personal best or even
+						fastest lap."
+					>
+						<DriverMiniSectors driverDisplayName="test" sectors={exampleDriver.sectors} />
+					</HomeFeatureCard>
 
-					<div className={featureClass}>
-						<p className="text-lg font-medium leading-none">TBD</p>
-						<p className="leading-none text-gray-400">
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit dolores
-						</p>
-						<div className="rounded-lg bg-zinc-900 p-4">
-							<DriverInfo laps={exampleDriver.laps} status={exampleDriver.status} />
+					<HomeFeatureCard title="Driver Status" description="See at a glace if a driver has DRS or is in the pits">
+						<div className="w-16">
+							<DriverDRS driverStatus={exampleDriver.status} on possible />
 						</div>
-					</div>
+						<div className="w-16">
+							<DriverDRS driverStatus={"PIT"} on={false} possible={false} />
+						</div>
+
+						<DriverInfo laps={exampleDriver.laps} status={exampleDriver.status} />
+					</HomeFeatureCard>
+
+					<HomeFeatureCard
+						title="Track Map"
+						description="See the postion of the drivers in realtime. Where and if drivers crashed and their status."
+					>
+						<Image src={mapFeature} alt="" className="rounded-lg" />
+					</HomeFeatureCard>
 				</div>
 			</div>
 
