629a316aa10c6989b41e252c666c169820380a65
Slowlydev <slowly.developers@gmail.com>
2023-11-03 18:44:20 +0100

    feat(dash): impelemnt image of head-to-head

--- dash/src/app/(socket)/head-to-head/page.tsx
+++ dash/src/app/(socket)/head-to-head/page.tsx
@@ -7,28 +7,45 @@ import { useSocket } from "../../../context/SocketContext";
 import { DriverType } from "../../../types/driver.type";
 
 export default function HeadToHeadPage() {
-  const { state } = useSocket();
+	const { state } = useSocket();
 
-  const [selectedDrivers, setSelectedDrivers] = useState<DriverType[]>([]);
+	const [selectedDriverNumbers, setSelectedDriverNumbers] = useState<DriverType["nr"][]>([]);
+	const selectedDrivers: DriverType[] = state?.drivers
+		? state.drivers.filter((driver) => selectedDriverNumbers.find((driverNr) => driverNr === driver.nr))
+		: [];
 
-  return (
-    <div className="mt-2 w-full">
-      {state && state.drivers && (
-        <div className="flex gap-2">
-          {state.drivers.map((driver) => (
-            <div key={`driver.selector.${driver.short}`}>
-              <DriverTag teamColor={driver.teamColor} short={driver.short} />
-            </div>
-          ))}
-        </div>
-      )}
+	const toggleDriver = (nr: DriverType["nr"]) => {
+		setSelectedDriverNumbers((old) => {
+			if (old.find((dNr) => dNr === nr)) {
+				return old.filter((dNr) => dNr !== nr);
+			} else {
+				return [...old, nr];
+			}
+		});
+	};
 
-      {state && state.drivers && (
-        <div className="grid grid-cols-2">
-          <HeadToHeadDriver driver={state.drivers[0]} />
-          <HeadToHeadDriver driver={state.drivers[1]} />
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="mt-2 w-full">
+			{state && state.drivers && (
+				<div className="flex gap-2">
+					{state.drivers.map((driver) => (
+						<div
+							key={`driver.selector.${driver.short}`}
+							onClick={() => toggleDriver(driver.nr)}
+							className="cursor-pointer"
+						>
+							<DriverTag teamColor={driver.teamColor} short={driver.short} />
+						</div>
+					))}
+				</div>
+			)}
+
+			{selectedDrivers.length > 1 && (
+				<div className="grid grid-cols-2">
+					<HeadToHeadDriver driver={selectedDrivers[0]} />
+					<HeadToHeadDriver driver={selectedDrivers[1]} />
+				</div>
+			)}
+		</div>
+	);
 }
--- dash/src/components/HeadToHeadDriver.tsx
+++ dash/src/components/HeadToHeadDriver.tsx
@@ -3,21 +3,23 @@ import Image from "next/image";
 import type { DriverType } from "../types/driver.type";
 
 type Props = {
-  driver: DriverType;
+	driver: DriverType;
 };
 
 export default function HeadToHeadDriver({ driver }: Props) {
-  return (
-    <div>
-      <div>
-        {/* <Image src={driver} /> */}
-        <div>
-          <p>
-            {driver.firstName} {driver.lastName}
-          </p>
-          <p>{driver.teamName}</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				<Image src={driver.image} alt={driver.firstName} width={200} height={200} />
+				<div>
+					<p>
+						{driver.firstName} {driver.lastName}
+					</p>
+					<p>{driver.teamName}</p>
+				</div>
+
+				<pre className="whitespace-pre-wrap">{JSON.stringify(driver)}</pre>
+			</div>
+		</div>
+	);
 }
