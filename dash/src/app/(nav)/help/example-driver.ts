be5f184c669d2614e30f8f658ebedffa39a11c90
Slowlydev <slowly.developers@gmail.com>
2023-11-09 22:13:57 +0100

    refactor(dash): adjust tire help

--- dash/src/app/(nav)/help/example-driver.ts
+++ dash/src/app/(nav)/help/example-driver.ts
@@ -66,7 +66,7 @@ export const exampleDriver: DriverType = {
 			new: false,
 		},
 		{
-			compound: "medium",
+			compound: "soft",
 			laps: 4,
 			new: false,
 		},
--- dash/src/components/HelpTires.tsx
+++ dash/src/components/HelpTires.tsx
@@ -15,42 +15,49 @@ export default function HelpTires() {
 				Tires <span className="text-gray-500">(you probably know them)</span>
 			</h3>
 
+			<p>
+				In formula 1 there are multiple tires which a drivers/teams can choose from. Below you can learn more about
+				them. In the dashboard you will find the culster of infomration about the current tire like shown just right
+				below.
+			</p>
+
+			<div className="flex items-center gap-2 py-4">
+				<div className="w-[9rem]">
+					<DriverTire stints={exampleDriver.stints} />
+				</div>
+
+				<p className="whitespace-break-spaces text-gray-500">
+					The driver is current on a <span className="text-white">used (*)</span>,{" "}
+					<span className="text-white">Soft tire (icon)</span>, he has done{" "}
+					<span className="text-white">3 Laps (L)</span> on that Tire in the current Stint, also he has{" "}
+					<span className="text-white">changed tires 1 time (St.)</span> in this session.
+				</p>
+			</div>
+
 			<div className="flex flex-col gap-2">
 				<div className="flex items-center gap-2">
-					<Image src={softTireIcon} alt="soft" className="h-10 w-10" />
+					<Image src={softTireIcon} alt="soft" className="h-8 w-8" />
 					<p>Soft (usually fastest)</p>
 				</div>
 
 				<div className="flex items-center gap-2">
-					<Image src={mediumTireIcon} alt="medium" className="h-10 w-10" />
+					<Image src={mediumTireIcon} alt="medium" className="h-8 w-8" />
 					<p>Medium</p>
 				</div>
 
 				<div className="flex items-center gap-2">
-					<Image src={hardTireIcon} alt="hard" className="h-10 w-10" />
+					<Image src={hardTireIcon} alt="hard" className="h-8 w-8" />
 					<p>Hard (usually slower but lasts longer)</p>
 				</div>
 
 				<div className="flex items-center gap-2">
-					<Image src={intermediateTireIcon} alt="intermediates" className="h-10 w-10" />
+					<Image src={intermediateTireIcon} alt="intermediates" className="h-8 w-8" />
 					<p>Intermediate (for rain)</p>
 				</div>
 
 				<div className="flex items-center gap-2">
-					<Image src={wetTireIcon} alt="wets" className="h-10 w-10" />
+					<Image src={wetTireIcon} alt="wets" className="h-8 w-8" />
 					<p>Wets (for heavy rain)</p>
-				</div>
-				<div className="flex items-center gap-2 pt-4">
-					<DriverTire stints={exampleDriver.stints} />
-					<p>* (previously used tire)</p>
-				</div>
-				<div className="flex items-center gap-2">
-					<DriverTire stints={[{ compound: "soft", laps: 2, new: true }]} />
-					<p>Stint (count of pit-stops)</p>
-				</div>
-				<div className="flex items-center gap-2">
-					<DriverTire stints={[{ compound: "hard", laps: 0, new: true }]} />
-					<p>Laps (current laps done on this tire)</p>
 				</div>
 			</div>
 		</div>
