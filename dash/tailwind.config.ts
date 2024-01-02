9cb31537d98cc089690bf154a88a25ac34c43304
Slowlydev <slowly.developers@gmail.com>
2024-01-02 17:08:58 +0100

    feat: add complications for weather

--- dash/tailwind.config.ts
+++ dash/tailwind.config.ts
@@ -1,13 +1,13 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      screens: {
-        "3xl": "1800px",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			screens: {
+				"3xl": "1800px",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
