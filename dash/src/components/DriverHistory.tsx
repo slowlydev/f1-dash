61133c0635422aa06223dc469f26cae84beeabbb
Slowlydev <slowly.developers@gmail.com>
2024-01-29 22:56:39 +0100

    feat: add history driver

---
+++ dash/src/components/DriverHistory.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import { DriverType } from "@/types/state.type";
+import Graph from "./Graph";
+import DriverHistoryTires from "./DriverHistoryTires";
+
+type Props = {
+	driver: DriverType;
+};
+
+export default function DriverHistory({ driver }: Props) {
+	return (
+		<>
+			{driver.gapHistory && driver.sectorHisotry && driver.laptimeHistory && (
+				<motion.div
+					key="additonal"
+					className="grid items-center gap-1"
+					style={{
+						gridTemplateColumns: "6rem 13.5rem 5rem 5rem 6rem 6rem 6rem",
+					}}
+					initial={{ opacity: 0 }}
+					exit={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+				>
+					<DriverHistoryTires stints={driver.stints} />
+
+					<div className="text-sm font-medium text-zinc-600">
+						<p>Expected to Box in 3 Laps</p>
+						<p>Averge Pit Stop Time: 22s</p>
+						<p>Expected to re-join 8th</p>
+					</div>
+
+					<div>
+						<Graph values={driver.gapHistory} lines={13} />
+					</div>
+					<div>
+						<Graph values={driver.laptimeHistory} lines={13} />
+					</div>
+
+					{driver.sectorHisotry.map((sector, i) => (
+						<div key={`driver.${driver.nr}.sector.hsitory.${i}`}>
+							<Graph values={sector} lines={13} />
+						</div>
+					))}
+				</motion.div>
+			)}
+		</>
+	);
+}
---
+++ dash/src/components/DriverHistoryTires.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Image from "next/image";
+
+import { Stint } from "@/types/state.type";
+
+type Props = {
+	stints: Stint[];
+};
+
+export default function DriverHistoryTires({ stints }: Props) {
+	const unknownCompound = (stint: Stint) =>
+		!["soft", "medium", "hard", "intermediate", "wet"].includes(stint.compound ?? "");
+
+	return (
+		<div className="flex flex-row items-center justify-start gap-1">
+			{stints.map((stint, i) => (
+				<div className="flex flex-col items-center gap-1">
+					{unknownCompound(stint) && (
+						<div className="flex h-8 w-8 items-center justify-center">
+							<p>?</p>
+						</div>
+					)}
+					{!unknownCompound(stint) && (
+						<Image src={`/tires/${stint.compound.toLowerCase()}.svg`} width={32} height={32} alt={stint.compound} />
+					)}
+
+					<p className="text-sm font-bold leading-none text-zinc-600">{stint.laps} L</p>
+				</div>
+			))}
+
+			{stints.length < 1 && (
+				<>
+					<LoadingTire />
+					<LoadingTire />
+				</>
+			)}
+		</div>
+	);
+}
+
+function LoadingTire() {
+	return (
+		<div className="flex flex-col items-center gap-1">
+			<div className="h-8 w-8 animate-pulse rounded-full bg-gray-700" />
+			<div className="h-4 w-8 animate-pulse rounded-md  bg-gray-700" />
+		</div>
+	);
+}
