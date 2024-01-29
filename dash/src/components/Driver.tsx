398cb3dc6833d872a5c49c11248ecf64ec8a1b68
Slowlydev <slowly.developers@gmail.com>
2024-01-29 22:56:49 +0100

    feat: add history to driver

--- dash/src/components/Driver.tsx
+++ dash/src/components/Driver.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import clsx from "clsx";
-import { motion } from "framer-motion";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 import DriverTag from "./DriverTag";
 import DriverDRS from "./DriverDRS";
@@ -10,7 +11,7 @@ import DriverTire from "./DriverTire";
 import DriverMiniSectors from "./DriverMiniSectors";
 import DriverLapTime from "./DriverLapTime";
 import DriverInfo from "./DriverInfo";
-import { useState } from "react";
+import DriverHistory from "./DriverHistory";
 
 import { DriverType } from "@/types/state.type";
 
@@ -23,61 +24,36 @@ export default function Driver({ driver, position }: Props) {
 	const [open, setOpen] = useState<boolean>(false);
 
 	return (
-		<motion.div
+		<div
 			onClick={() => setOpen((old) => !old)}
 			className={clsx("px-2", {
 				"opacity-50": driver.status === "OUT" || driver.status === "RETIRED" || driver.status === "STOPPED",
 				"bg-indigo-800 bg-opacity-30": driver.lapTimes.best.fastest,
 				"bg-red-800 bg-opacity-30": false, // TODO use this for danger zone in quali
 			})}
-			layout
 		>
 			<motion.div
+				key="always"
 				className={clsx("h-18 grid items-center gap-1 py-1")}
 				style={{
 					gridTemplateColumns: "6rem 4rem 5.5rem 4rem 5rem 5rem auto auto",
 				}}
+				layout
 			>
-				{/* <DriverPosChanage positionChange={driver.positionChange} /> */}
-
 				<DriverTag className="!min-w-[5.5rem]" short={driver.short} teamColor={driver.teamColor} position={position} />
-
 				<DriverDRS on={driver.drs.on} possible={driver.drs.possible} driverStatus={driver.status} />
-
 				<DriverTire stints={driver.stints} />
-
 				<DriverInfo status={driver.status} laps={driver.laps} />
-
 				<DriverGap toFront={driver.gapToFront} toLeader={driver.gapToLeader} catching={driver.catchingFront} />
-
 				<DriverLapTime last={driver.lapTimes.last} best={driver.lapTimes.best} />
-
 				<DriverMiniSectors sectors={driver.sectors} driverDisplayName={driver.short} />
 			</motion.div>
 
-			{open && (
-				<motion.div
-					className="grid gap-2"
-					style={{
-						gridTemplateColumns: "6rem 8rem 6rem 6rem 6rem 6rem 6rem",
-					}}
-				>
-					<div>tires</div>
-
-					<div>
-						<p>analytics 1</p>
-						<p>analytics 2</p>
-						<p>analytics 3</p>
-					</div>
-
-					<div>graph</div>
-					<div>graph</div>
-
-					<div>graph</div>
-					<div>graph</div>
-					<div>graph</div>
-				</motion.div>
-			)}
-		</motion.div>
+			<AnimatePresence>
+				{open && driver.gapHistory && driver.sectorHisotry && driver.laptimeHistory && (
+					<DriverHistory driver={driver} />
+				)}
+			</AnimatePresence>
+		</div>
 	);
 }
