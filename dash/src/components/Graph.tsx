c37dc11ffbcf342f51fd2d323b552dbee8fcdf22
Slowlydev <slowly.developers@gmail.com>
2024-02-11 22:16:23 +0100

    refactor: improve graph and start try adding animations

--- dash/src/components/Graph.tsx
+++ dash/src/components/Graph.tsx
@@ -1,22 +1,27 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { clamping } from "../lib/circle";
+
 type Props = {
 	values: number[];
 	lines?: number;
 };
 
 export default function Graph({ lines = 19, values }: Props) {
-	const height = 45;
+	const height = 40;
+	const safeHight = height - 10;
 	const width = lines * 6 - 5; // 6 would be a full, but the last line would be cut
 
 	const maxValue = Math.max(...values);
-	const minValue = Math.min(...values);
 
 	const scaleValue = (v: number): number => {
-		return height - (v / maxValue) * height;
+		return clamping(v, safeHight, 0, maxValue);
 	};
 
 	return (
 		<div>
-			<svg
+			<motion.svg
 				width={width}
 				height={height}
 				viewBox={`0 0 ${width} ${height}`}
@@ -28,20 +33,22 @@ export default function Graph({ lines = 19, values }: Props) {
 					.map((_, i) => (
 						<>
 							{i % 6 ? (
-								<rect x={i * 6} width="1" height={height - 10} className="fill-zinc-700" key={`short.${i}`} />
+								<rect x={i * 6} width="1" height={safeHight} className="fill-zinc-700" key={`short.${i}`} />
 							) : (
 								<rect x={i * 6} width="1" height={height} className="fill-zinc-700" key={`long.${i}`} />
 							)}
 						</>
 					))}
 
-				<g clip-path="url(#gradient-clip)">
-					<path
-						d={`M0 ${scaleValue(values[0])} ${values.map((v, i) => `L${i * 6} ${scaleValue(v)}`).join(" ")} L${values.length * 6 + 1} ${scaleValue(values[values.length - 1])} L${width + 1} ${height} L0 ${height} Z`}
+				<g>
+					<motion.path
+						layout
+						d={`M0 ${scaleValue(values[0])} ${values.map((v, i) => `L${i * 6} ${scaleValue(v)}`).join(" ")} L${values.length * 6} ${scaleValue(values[values.length - 1])} L${values.length * 6} ${height} L0 ${height} Z`}
 						fill="url(#green-gradient)"
 					/>
-					<path
-						d={`M0 ${scaleValue(values[0])} ${values.map((v, i) => `L${i * 6} ${scaleValue(v)}`).join(" ")} L${values.length * 6 + 1} ${scaleValue(values[values.length - 1])} L${width + 1} ${height}`}
+					<motion.path
+						layout
+						d={`M0 ${scaleValue(values[0])} ${values.map((v, i) => `L${i * 6} ${scaleValue(v)}`).join(" ")} L${values.length * 6 + 1} ${scaleValue(values[values.length - 1])}`}
 						stroke="#34C85A"
 						strokeWidth="1.5"
 					/>
@@ -54,17 +61,13 @@ export default function Graph({ lines = 19, values }: Props) {
 						x1={width / 2}
 						y1={0}
 						x2={width / 2}
-						y2={height}
+						y2={safeHight}
 					>
 						<stop offset={0} stopColor="rgb(16, 185, 129)" stopOpacity="0.5" />
 						<stop offset={1} stopColor="rgb(16, 185, 129)" stopOpacity="0" />
 					</linearGradient>
-
-					<clipPath id="gradient-clip">
-						<rect width={width} height={height - 10} fill="white" />
-					</clipPath>
 				</defs>
-			</svg>
+			</motion.svg>
 		</div>
 	);
 }
