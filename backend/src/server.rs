9e399987387bdd0adec4a55aea3f7f52de2fa35c
Slowlydev <slowly.developers@gmail.com>
2024-02-19 22:39:15 +0100

    refactor(server): use broadcast again and move full code into mod

--- backend/src/server.rs
+++ backend/src/server.rs
@@ -1,22 +1,43 @@
 use futures::StreamExt;
 
 use std::net::SocketAddr;
-use tokio::{net::TcpStream, sync::mpsc::UnboundedSender};
+use tokio::{
+    net::{TcpListener, TcpStream},
+    sync::mpsc::UnboundedSender,
+};
 
 use tokio_tungstenite::{tungstenite::Message, WebSocketStream};
-use tracing::debug;
+use tracing::{debug, error, info};
 
-use crate::server_manager::{self, BroadcastEvents};
+use crate::broadcast;
 
-pub async fn listen(
+pub async fn listen(broadcast_tx: UnboundedSender<broadcast::Event>) {
+    let addr = "127.0.0.1:4000".to_string();
+    let listener = TcpListener::bind(&addr).await.expect("TCP failed");
+
+    let mut id: u32 = 0;
+
+    while let Ok((stream, addr)) = listener.accept().await {
+        match tokio_tungstenite::accept_async(stream).await {
+            Err(e) => error!("Websocket connection error : {}", e),
+            Ok(stream) => {
+                id += 1;
+                info!("new connection: {} {}", addr, id);
+                tokio::spawn(accept(stream, addr, id, broadcast_tx.clone()));
+            }
+        }
+    }
+}
+
+async fn accept(
     stream: WebSocketStream<TcpStream>,
     addr: SocketAddr,
     id: u32,
-    broadcast_sender: UnboundedSender<BroadcastEvents>,
+    broadcast_tx: UnboundedSender<broadcast::Event>,
 ) {
     let (sender, mut receiver) = stream.split();
-    let conn = server_manager::Connection::new(id, addr, sender);
-    let _ = broadcast_sender.send(BroadcastEvents::Join(conn));
+    let conn = broadcast::Connection::new(id, addr, sender);
+    let _ = broadcast_tx.send(broadcast::Event::Join(conn));
 
     while let Some(Ok(msg)) = receiver.next().await {
         match msg {
@@ -24,11 +45,14 @@ pub async fn listen(
                 debug!("got close frame, aborting");
                 break;
             }
+            Message::Text(txt) => {
+                println!("{txt:?}");
+            }
             _ => {}
         }
     }
 
-    broadcast_sender.send(BroadcastEvents::Quit(id)).unwrap();
+    let _ = broadcast_tx.send(broadcast::Event::Quit(id));
 
     debug!("we are gonna disconnect, hopefully");
 }
