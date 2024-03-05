11f2cf19e91e4b1cc82e44126dddd4fb6f534c67
Slowlydev <slowly.developers@gmail.com>
2024-03-05 17:31:06 +0100

    refactor(delay-input): to accept a value

--- dash/src/components/DelayInput.tsx
+++ dash/src/components/DelayInput.tsx
@@ -1,30 +1,35 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { useEffect, useState, type FormEvent } from "react";
 
 type Props = {
+	delay: number;
 	setDebouncedDelay: (delay: number) => void;
 };
 
-export default function DelayInput({ setDebouncedDelay }: Props) {
-	const [delay, setDelay] = useState("");
+export default function DelayInput({ delay, setDebouncedDelay }: Props) {
+	const [delayI, setDelayI] = useState<string>("");
 
 	const updateDebounced = () => {
-		const nextDelay = delay ? parseInt(delay) : 0;
+		const nextDelay = delayI ? parseInt(delayI) : 0;
 		if (nextDelay < 0) return;
 		setDebouncedDelay(nextDelay);
 	};
 
-	const handleSubmit = (event: FormEvent) => {
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		updateDebounced();
 	};
 
+	useEffect(() => {
+		setDelayI(delay.toString());
+	}, [delay]);
+
 	return (
 		<form className="flex rounded-lg bg-zinc-800 p-2" onSubmit={handleSubmit}>
 			<input
-				value={delay}
-				onChange={(e) => setDelay(e.target.value)}
+				value={delayI}
+				onChange={(e) => setDelayI(e.target.value)}
 				onBlur={() => updateDebounced()}
 				placeholder="0s"
 				className="w-16 bg-zinc-800 text-center leading-none text-white placeholder-white"
