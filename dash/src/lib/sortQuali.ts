36b56b8ac8c5d9fd8ddaf3009ed3451870d55307
Slowlydev <slowly.developers@gmail.com>
2024-03-11 17:27:45 +0100

    feat: better quali sort

--- dash/src/lib/sortQuali.ts
+++ dash/src/lib/sortQuali.ts
@@ -5,8 +5,8 @@ type PosObject = {
 };
 
 export const sortQuali = (a: PosObject, b: PosObject) => {
-	const aPassed = a.sectors[2].segments.filter((s) => s.status > 0);
-	const bPassed = b.sectors[2].segments.filter((s) => s.status > 0);
+	const aPassed = a.sectors.flatMap((sector) => sector.segments).filter((s) => s.status > 0);
+	const bPassed = b.sectors.flatMap((sector) => sector.segments).filter((s) => s.status > 0);
 
 	return bPassed.length - aPassed.length;
 };
