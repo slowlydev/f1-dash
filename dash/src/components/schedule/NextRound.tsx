b065673ea14b3c08b62bd0584fc38925e0019631
Slowlydev <61624214+slowlydev@users.noreply.github.com>
2024-04-05 23:28:14 +0200

    fix: keys missing

--- dash/src/components/schedule/NextRound.tsx
+++ dash/src/components/schedule/NextRound.tsx
@@ -47,14 +47,14 @@ export default async function NextRound({ next }: Props) {
 
 				<div className="grid grid-cols-3 gap-8 pt-2">
 					{groupSessionByDay(next.sessions).map((day, i) => (
-						<div className="flex flex-col">
+						<div className="flex flex-col" key={`next.round.day.${i}`}>
 							<p>{utc(day.date).local().format("dddd")}</p>
 
 							<div className="grid grid-rows-2 gap-2">
 								{day.sessions.map((session, j) => (
 									<div
 										className={clsx("flex flex-col", utc(session.end).isBefore(now()) && "opacity-50")}
-										key={`next.day.${i}.session.${j}`}
+										key={`next.round.day.${i}.session.${j}`}
 									>
 										<p className="w-28 overflow-hidden text-ellipsis whitespace-nowrap sm:w-auto">{session.kind}</p>
 
