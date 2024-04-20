859f9796b5cc6572f4271c3a6abdc0a7cac06eb4
Slowlydev <61624214+slowlydev@users.noreply.github.com>
2024-04-20 11:29:49 +0200

    fix(root-layout): remove merge artifacts

--- dash/src/app/layout.tsx
+++ dash/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { type ReactNode } from "react";
-import { cookies } from "next/headers";
 import Script from "next/script";
 
 import "@/styles/globals.css";
@@ -12,13 +11,8 @@ import { GeistSans } from "geist/font/sans";
 export { metadata } from "@/metadata";
 export { viewport } from "@/viewport";
 
-import InfoBanner from "@/components/InfoBanner";
-
 export default function RootLayout({ children }: { children: ReactNode }) {
 	const enableTracking = !!env.NEXT_PUBLIC_ENABLE_TRACKING;
-
-	const cookiesStore = cookies();
-	const showBanner = cookiesStore.get("info-banner")?.value !== "hidden";
 
 	return (
 		<html lang="en" className={`${GeistSans.variable} ${GeistMono.variable} bg-zinc-950 font-sans text-white`}>
@@ -33,11 +27,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 				/>
 			)}
 
-			<body>
-				{showBanner && <InfoBanner />}
-
-				<div className="p-3">{children}</div>
-			</body>
+			<body>{children}</body>
 		</html>
 	);
 }
