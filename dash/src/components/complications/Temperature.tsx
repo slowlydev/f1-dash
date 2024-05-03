96d78e50fe6b385a72e8870a5227b025de373a23
Slowlydev <61624214+slowlydev@users.noreply.github.com>
2024-05-03 23:11:38 +0200

    fix: track temp can be high in miami

--- dash/src/components/complications/Temperature.tsx
+++ dash/src/components/complications/Temperature.tsx
@@ -2,13 +2,13 @@ import Gauge from "./Gauge";
 
 type Props = {
 	value: number;
-	label: string;
+	label: "TRC" | "AIR";
 };
 
 export default function TemperatureComplication({ value, label }: Props) {
 	return (
 		<div className="flex h-[55px] w-[55px] items-center justify-center rounded-full bg-black">
-			<Gauge value={value} max={40} gradient="temperature" />
+			<Gauge value={value} max={label === "TRC" ? 60 : 40} gradient="temperature" />
 
 			<div className="mt-2 flex flex-col items-center gap-0.5">
 				<p className="flex h-[22px] shrink-0 text-xl font-medium not-italic leading-[normal] text-[color:var(--Base-Text,#F2F2F2)]">
