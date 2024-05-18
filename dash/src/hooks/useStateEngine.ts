faf55452674459e30f9afea65aa6d4388b96d895
Slowlydev <61624214+slowlydev@users.noreply.github.com>
2024-05-18 15:17:34 +0200

    perf: no frezzing hopefully and bound to set time between updates

--- dash/src/hooks/useStateEngine.ts
+++ dash/src/hooks/useStateEngine.ts
@@ -8,16 +8,48 @@ type Frame<T> = {
 const sortFrames = <T>(a: Frame<T>, b: Frame<T>) => a.timestamp - b.timestamp;
 
 const MAX_BUFFER = 1000;
+const UPDATE_MS = 30;
 
 export const useStateEngine = <T>(name?: string) => {
 	const [state, setState] = useState<T | null>(null);
 	const [maxDelay, setMaxDelay] = useState<number>(0);
 
-	const broadcastChannelRef = useRef<BroadcastChannel>(name ? new BroadcastChannel(name) : null);
-	const runningRef = useRef<boolean>(true);
-	const internalDelayRef = useRef<number>(0);
+	const broadcastRef = useRef<BroadcastChannel>(name ? new BroadcastChannel(name) : null);
+
 	const bufferRef = useRef<Frame<T>[]>([]);
+	const runningRef = useRef<boolean>(true);
+	const delayRef = useRef<number>(0);
+
 	const requestRef = useRef<number | null>(null);
+	const lastRef = useRef<number>(0);
+
+	const animateNextFrame = (time: number) => {
+		if (time - lastRef.current > UPDATE_MS) {
+			if (runningRef.current) {
+				const buffer = bufferRef.current;
+
+				const lastFrame =
+					delayRef.current === 0
+						? buffer[buffer.length - 1]
+						: buffer.find((frame) => frame.timestamp > Date.now() - delayRef.current * 1000);
+
+				if (lastFrame) {
+					setState(lastFrame.data);
+					broadcastRef.current?.postMessage(lastFrame.data);
+				}
+			}
+
+			lastRef.current = time;
+			setMaxDelay(bufferRef.current.length > 0 ? Math.floor((Date.now() - bufferRef.current[0].timestamp) / 1000) : 0);
+		}
+
+		requestRef.current = requestAnimationFrame(animateNextFrame);
+	};
+
+	useEffect(() => {
+		requestRef.current = requestAnimationFrame(animateNextFrame);
+		return () => (requestRef.current ? cancelAnimationFrame(requestRef.current) : void 0);
+	}, []);
 
 	const addFrame = (data: T) => {
 		bufferRef.current.push({ data, timestamp: Date.now() });
@@ -28,7 +60,7 @@ export const useStateEngine = <T>(name?: string) => {
 		}
 	};
 
-	const addFramesWithTimestamp = (data: { data: T; timestamp: number }[]) => {
+	const addFramesWithTimestamp = (data: Frame<T>[]) => {
 		bufferRef.current.push(...data);
 		bufferRef.current.sort(sortFrames);
 
@@ -37,33 +69,7 @@ export const useStateEngine = <T>(name?: string) => {
 		}
 	};
 
-	const animateNextFrame = () => {
-		if (runningRef.current) {
-			const buffer = bufferRef.current;
-
-			const lastFrame =
-				internalDelayRef.current === 0
-					? buffer[buffer.length - 1]
-					: buffer.find((frame) => frame.timestamp > Date.now() - internalDelayRef.current * 1000);
-
-			if (lastFrame) {
-				setState(lastFrame.data);
-				broadcastChannelRef.current?.postMessage(lastFrame.data);
-			}
-		}
-
-		setMaxDelay(bufferRef.current.length > 0 ? Math.floor((Date.now() - bufferRef.current[0].timestamp) / 1000) : 0);
-		requestRef.current = requestAnimationFrame(animateNextFrame);
-	};
-
-	useEffect(() => {
-		requestRef.current = requestAnimationFrame(animateNextFrame);
-		return () => (requestRef.current ? cancelAnimationFrame(requestRef.current) : void 0);
-	}, []);
-
-	const setDelay = (delay: number) => {
-		internalDelayRef.current = delay;
-	};
+	const setDelay = (delay: number) => (delayRef.current = delay);
 
 	const pause = () => (runningRef.current = false);
 	const resume = () => (runningRef.current = true);
