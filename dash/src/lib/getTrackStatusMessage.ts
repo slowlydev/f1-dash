8fcc8a51763c61d85d26c3c879ea1198ad27bce3
Slowlydev <me@slowly.dev>
2024-10-19 23:14:56 +0200

    refactor(map): useMemo and cleanup code

--- dash/src/lib/getTrackStatusMessage.ts
+++ dash/src/lib/getTrackStatusMessage.ts
@@ -1,4 +1,4 @@
-type StatusMessage = {
+export type StatusMessage = {
 	message: string;
 	color: string;
 	trackColor: string;
@@ -11,22 +11,22 @@ type MessageMap = {
 	[key: string]: StatusMessage;
 };
 
-export const getTrackStatusMessage = (statusCode: number | undefined): StatusMessage | null => {
-	const messageMap: MessageMap = {
-		1: { message: "Track Clear", color: "bg-emerald-500", trackColor: "stroke-white", hex: "#34b981" },
-		2: {
-			message: "Yellow Flag",
-			color: "bg-yellow-500",
-			trackColor: "stroke-yellow-500",
-			bySector: true,
-			hex: "#f59e0c",
-		},
-		3: { message: "Flag", color: "bg-yellow-500", trackColor: "stroke-yellow-500", bySector: true, hex: "#f59e0c" },
-		4: { message: "Safety Car", color: "bg-yellow-500", trackColor: "stroke-yellow-500", hex: "#f59e0c" },
-		5: { message: "Red Flag", color: "bg-red-500", trackColor: "stroke-red-500", hex: "#ef4444" },
-		6: { message: "VSC Deployed", color: "bg-yellow-500", trackColor: "stroke-yellow-500", hex: "#f59e0c" },
-		7: { message: "VSC Ending", color: "bg-yellow-500", trackColor: "stroke-yellow-500", hex: "#f59e0c" },
-	};
+const MESSAGE_MAP: MessageMap = {
+	1: { message: "Track Clear", color: "bg-emerald-500", trackColor: "stroke-white", hex: "#34b981" },
+	2: {
+		message: "Yellow Flag",
+		color: "bg-yellow-500",
+		trackColor: "stroke-yellow-500",
+		bySector: true,
+		hex: "#f59e0c",
+	},
+	3: { message: "Flag", color: "bg-yellow-500", trackColor: "stroke-yellow-500", bySector: true, hex: "#f59e0c" },
+	4: { message: "Safety Car", color: "bg-yellow-500", trackColor: "stroke-yellow-500", hex: "#f59e0c" },
+	5: { message: "Red Flag", color: "bg-red-500", trackColor: "stroke-red-500", hex: "#ef4444" },
+	6: { message: "VSC Deployed", color: "bg-yellow-500", trackColor: "stroke-yellow-500", hex: "#f59e0c" },
+	7: { message: "VSC Ending", color: "bg-yellow-500", trackColor: "stroke-yellow-500", hex: "#f59e0c" },
+} as const;
 
-	return statusCode ? messageMap[statusCode] ?? messageMap[0] : null;
+export const getTrackStatusMessage = (statusCode: number | undefined): StatusMessage | null => {
+	return statusCode ? MESSAGE_MAP[statusCode] ?? MESSAGE_MAP[0] : null;
 };
