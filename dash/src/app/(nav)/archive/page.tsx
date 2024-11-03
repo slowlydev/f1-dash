9764882dbf4d654dbfea4d27c0e017cbfd517796
dev-viinz <42003446+dev-viinz@users.noreply.github.com>
2024-11-03 19:48:19 +0100

    feat: finish design draft + add start and end date of event

--- dash/src/app/(nav)/archive/page.tsx
+++ dash/src/app/(nav)/archive/page.tsx
@@ -1,4 +1,6 @@
 import { env } from "@/env.mjs";
+import { utc } from "moment";
+import Footer from "@/components/Footer"; // Adjust the import path as necessary
 
 type Meeting = {
 	key: number;
@@ -8,8 +10,12 @@ type Meeting = {
 	country: {
 		name: string;
 	};
-	sessions: {};
+	sessions: {
+		startDate: string;
+		endDate: string;
+	}[];
 };
+
 const getArchiveForYear = async (year: string): Promise<Meeting[] | null> => {
 	try {
 		const nextReq = await fetch(`${env.NEXT_PUBLIC_API_URL}/api/archive/${year}`, {
@@ -28,26 +34,48 @@ export default async function ArchivePage({
 	searchParams: { [key: string]: string | string[] | undefined };
 }) {
 	let year = searchParams["year"];
-	//const searchParams = useSearchParams();
-	//let year = searchParams.get("year");
 	const currentYear = new Date(Date.now()).getFullYear().toString();
 	if (year == null || year < "2017" || year > currentYear || typeof year !== "string") {
 		year = currentYear;
 	}
 	const archive = await getArchiveForYear(year);
+
+	if (!archive) {
+		return (
+			<div className="flex h-44 flex-col items-center justify-center">
+				<p>No archive data found for {year}</p>
+			</div>
+		);
+	}
+
 	return (
-		<div>
-			<h1>{archive?.length}</h1>
-			<ul>
-				{archive?.map((meet) => (
-					<li className="border" key={meet.key}>
-						<div className="flex flex-col">
-							<div>{meet.officialName}</div>
-							<div>{meet.country.name}</div>
+		<div className="container mx-auto max-w-screen-lg px-4 pb-8">
+			<div className="my-4">
+				<h1 className="text-3xl font-bold">Archive for {year}</h1>
+				<p className="text-zinc-600">All times are local time</p>
+			</div>
+			<ul className="grid grid-cols-1 gap-8 md:grid-cols-2">
+				{archive.map((meet) => (
+					<li className="rounded-md border border-zinc-700 p-4 shadow-md" key={meet.key}>
+						<div className="flex h-full flex-col justify-between">
+							<div>
+								<h2 className="text-xl font-bold text-white">{meet.officialName}</h2>
+								<p className="text-sm text-zinc-500">{meet.country.name}</p>
+								<p className="mt-1 text-sm italic text-zinc-400">{meet.location}</p>
+							</div>
+							<div className="mt-2">
+								<p className="text-sm text-zinc-600">
+									{utc(meet.sessions[0].startDate).local().format("MMMM D, YYYY")} -{" "}
+									{utc(meet.sessions[meet.sessions.length - 1].endDate)
+										.local()
+										.format("MMMM D, YYYY")}
+								</p>
+							</div>
 						</div>
 					</li>
 				))}
 			</ul>
+			<Footer />
 		</div>
 	);
 }
