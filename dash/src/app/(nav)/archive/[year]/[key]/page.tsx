57359895c24237817ea34b3916734fe53bd9946e
dev-viinz <42003446+dev-viinz@users.noreply.github.com>
2024-11-04 21:29:05 +0100

    feat: add details page

---
+++ dash/src/app/(nav)/archive/[year]/[key]/page.tsx
@@ -0,0 +1,71 @@
+import Footer from "@/components/Footer"; // Adjust the import path as necessary
+import { utc } from "moment";
+import { Meeting } from "@/types/archive.type";
+import Link from "next/link";
+import { env } from "@/env.mjs";
+
+const getArchiveForYear = async (year: string): Promise<Meeting[] | null> => {
+	try {
+		const nextReq = await fetch(`${env.NEXT_PUBLIC_API_URL}/api/archive/${year}`, {
+			next: { revalidate: 60 * 60 * 4 },
+		});
+		const schedule: Meeting[] = await nextReq.json();
+		return schedule;
+	} catch (e) {
+		return null;
+	}
+};
+
+export default async function MeetingDetailsPage({ params }: { params: { key: string; year: string } }) {
+	const { key, year } = params;
+	const archive = await getArchiveForYear(year);
+	const meeting = archive?.find((meet) => meet.key.toString() === key);
+
+	if (!meeting) {
+		return (
+			<div className="container mx-auto max-w-screen-lg px-4 pb-8">
+				<div className="flex h-44 flex-col items-center justify-center">
+					<p>No meeting details found for key: {key}</p>
+				</div>
+				<Footer />
+			</div>
+		);
+	}
+
+	return (
+		<div className="container mx-auto max-w-screen-lg px-4 pb-8">
+			<Link href={`/archive/${year}`}>
+				<div className="mt-4 text-blue-500 hover:underline">← Back to Year Overview</div>
+			</Link>
+			<div className="my-4">
+				<h1 className="text-3xl font-bold">{meeting.officialName}</h1>
+				<p className="text-sm text-zinc-500">{meeting.country.name}</p>
+				<p className="mt-1 text-sm italic text-zinc-400">{meeting.location}</p>
+				<p className="mt-2 text-sm text-zinc-600">
+					{utc(meeting.sessions[0].startDate).local().format("MMMM D, YYYY")} -{" "}
+					{utc(meeting.sessions[meeting.sessions.length - 1].endDate)
+						.local()
+						.format("MMMM D, YYYY")}
+				</p>
+				<div className="mt-4">
+					<h2 className="text-2xl font-bold">Sessions</h2>
+					<ul className="mt-2">
+						{meeting.sessions.map((session, index) => (
+							<li key={index} className="mb-4">
+								<h3 className="text-xl font-semibold">{session.name}</h3>
+								<p className="text-sm text-zinc-500">
+									{utc(session.startDate).local().format("MMMM D, YYYY")} -{" "}
+									{utc(session.endDate).local().format("MMMM D, YYYY")}
+								</p>
+								<p className="text-sm text-zinc-500">
+									{session.startDate} - {session.endDate}
+								</p>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
+}
---
+++ dash/src/app/(nav)/archive/[year]/page.tsx
@@ -0,0 +1,75 @@
+import { env } from "@/env.mjs";
+import Footer from "@/components/Footer"; // Adjust the import path as necessary
+import SegmentedLinks from "@/components/SegmentedLinks";
+import { utc } from "moment";
+import Link from "next/link";
+import { Meeting } from "@/types/archive.type";
+
+const getArchiveForYear = async (year: string): Promise<Meeting[] | null> => {
+	try {
+		const nextReq = await fetch(`${env.NEXT_PUBLIC_API_URL}/api/archive/${year}`, {
+			next: { revalidate: 60 * 60 * 4 },
+		});
+		const schedule: Meeting[] = await nextReq.json();
+		return schedule;
+	} catch (e) {
+		return null;
+	}
+};
+
+export default async function ArchivePage({ params }: { params: { year: string } }) {
+	const currentYear = new Date(Date.now()).getFullYear();
+	let year = params.year;
+	if (year == null || year < "2018" || year > currentYear.toString() || typeof year !== "string") {
+		year = currentYear.toString();
+	}
+	const archive = await getArchiveForYear(year);
+
+	const years = [];
+	for (let i = 2018; i <= currentYear; i++) {
+		years.push({ label: i.toString(), href: `/archive/${i.toString()}` });
+	}
+
+	return (
+		<div className="container mx-auto max-w-screen-lg px-4 pb-8">
+			<div className="my-4 flex items-center justify-between">
+				<h1 className="text-3xl font-bold">Archive for {year}</h1>
+				<SegmentedLinks id="year" selected={`/archive/${year}`} options={years} />
+			</div>
+			{!archive ? (
+				<div className="flex h-44 flex-col items-center justify-center">
+					<p>No archive data found for {year}</p>
+				</div>
+			) : (
+				<>
+					<p className="text-zinc-600">All times are local time</p>
+					<ul className="grid grid-cols-1 gap-8 md:grid-cols-2">
+						{archive.map((meet) => (
+							<li className="rounded-md border border-zinc-700 p-4 shadow-md" key={meet.key}>
+								<div className="flex h-full flex-col justify-between">
+									<div>
+										<h2 className="text-xl font-bold text-white">{meet.officialName}</h2>
+										<p className="text-sm text-zinc-500">{meet.country.name}</p>
+										<p className="mt-1 text-sm italic text-zinc-400">{meet.location}</p>
+									</div>
+									<div className="mt-2">
+										<p className="text-sm text-zinc-600">
+											{utc(meet.sessions[0].startDate).local().format("MMMM D, YYYY")} -{" "}
+											{utc(meet.sessions[meet.sessions.length - 1].endDate)
+												.local()
+												.format("MMMM D, YYYY")}
+										</p>
+									</div>
+									<Link href={`/archive/${year}/${meet.key}`}>
+										<div className="mt-2 text-blue-500 hover:underline">View Details</div>
+									</Link>
+								</div>
+							</li>
+						))}
+					</ul>
+				</>
+			)}
+			<Footer />
+		</div>
+	);
+}
