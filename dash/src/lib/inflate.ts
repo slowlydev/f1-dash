a2827039c6681423eea7811bcb25d48824699967
Slowlydev <me@slowly.dev>
2024-11-17 17:52:31 +0100

    refactor(inflate): use new inflate fn

--- dash/src/lib/inflate.ts
+++ dash/src/lib/inflate.ts
@@ -1,10 +1,17 @@
 import { inflateRaw } from "pako";
 
 export const inflate = <T>(data: string): T => {
-	return JSON.parse(
-		inflateRaw(
-			Uint8Array.from(atob(data), (c) => c.charCodeAt(0)),
-			{ to: "string" },
-		),
-	);
+	const binaryString = atob(data);
+
+	const len = binaryString.length;
+
+	const bytes = new Uint8Array(len);
+
+	for (let i = 0; i < len; i++) {
+		bytes[i] = binaryString.charCodeAt(i);
+	}
+
+	const inflatedData = inflateRaw(bytes, { to: "string" });
+
+	return JSON.parse(inflatedData);
 };
