34c14894f169c86b1f0a34f90d178c9f6e649265
Slowlydev <me@slowly.dev>
2025-02-22 17:53:21 +0100

    fix: gauges scroll

--- dash/src/components/complications/Humidity.tsx
+++ dash/src/components/complications/Humidity.tsx
@@ -10,11 +10,11 @@ type Props = {
 
 export default function HumidityComplication({ value }: Props) {
 	return (
-		<div className="flex h-[55px] w-[55px] items-center justify-center rounded-full bg-black">
+		<div className="relative flex h-[55px] w-[55px] items-center justify-center rounded-full bg-black">
 			<Gauge value={value} max={100} gradient="humidity" />
 
 			<div className="mt-2 flex flex-col items-center gap-0.5">
-				<p className="flex h-[22px] shrink-0 text-xl font-medium not-italic leading-[normal] text-[color:var(--Base-Text,#F2F2F2)]">
+				<p className="flex h-[22px] shrink-0 text-xl leading-[normal] font-medium text-[color:var(--Base-Text,#F2F2F2)] not-italic">
 					{value}
 				</p>
 				<Image src={humidityIcon} alt="humidity icon" className="h-[11px] w-auto" />
