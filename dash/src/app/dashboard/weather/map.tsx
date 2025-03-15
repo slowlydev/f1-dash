c4c99d9c5b20d17365b03ea6880110f2ee58dffa
Slowlydev <me@slowly.dev>
2025-03-15 20:35:59 +0100

    feat: better feel of weather steps

--- dash/src/app/dashboard/weather/map.tsx
+++ dash/src/app/dashboard/weather/map.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import maplibregl, { Map } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
@@ -17,6 +17,8 @@ import { useDataStore } from "@/stores/useDataStore";
 export function WeatherMap() {
 	const meeting = useDataStore((state) => state?.sessionInfo?.meeting);
 
+	const [loading, setLoading] = useState<boolean>(true);
+
 	const mapContainerRef = useRef<HTMLDivElement>(null);
 	const mapRef = useRef<Map>(null);
 
@@ -29,14 +31,20 @@ export function WeatherMap() {
 		const currentFrameIndex = currentFrameRef.current;
 		const nextFrameIndex = (currentFrameIndex + 1) % framesRef.current.length;
 
-		mapRef.current?.setLayoutProperty(`rainviewer-frame-${currentFrameIndex}`, "visibility", "none");
-		mapRef.current?.setLayoutProperty(`rainviewer-frame-${nextFrameIndex}`, "visibility", "visible");
+		// mapRef.current?.setLayoutProperty(`rainviewer-frame-${currentFrameIndex}`, "visibility", "none");
+		// mapRef.current?.setLayoutProperty(`rainviewer-frame-${nextFrameIndex}`, "visibility", "visible");
+
+		mapRef.current?.setPaintProperty(`rainviewer-frame-${currentFrameIndex}`, "raster-opacity", 0);
+		mapRef.current?.setPaintProperty(`rainviewer-frame-${nextFrameIndex}`, "raster-opacity", 0.8);
 
 		currentFrameRef.current = nextFrameIndex;
 	};
 
-	const handleMapLoad = (rainviewer: Rainviewer) => {
+	const handleMapLoad = async () => {
 		if (!mapRef.current) return;
+
+		const rainviewer = await getRainviewer();
+		if (!rainviewer) return;
 
 		framesRef.current = [...rainviewer.radar.past, ...rainviewer.radar.nowcast];
 
@@ -51,12 +59,12 @@ export function WeatherMap() {
 					tiles: [`${rainviewer.host}/${frame.path}/256/{z}/{x}/{y}/8/1_0.webp`],
 					tileSize: 256,
 				},
-				layout: {
-					visibility: "none",
-				},
+				// layout: {
+				// 	visibility: "none",
+				// },
 				paint: {
-					"raster-opacity": 0.8,
-					"raster-fade-duration": 0,
+					"raster-opacity": 0,
+					"raster-fade-duration": 200,
 					"raster-resampling": "nearest",
 				},
 			});
@@ -72,22 +80,25 @@ export function WeatherMap() {
 			if (!meeting) return;
 
 			const coords = await fetchCoords(`${meeting.country.name}, ${meeting.location}`);
-			if (!coords) return;
-
-			const rainviewer = await getRainviewer();
-			if (!rainviewer) return;
+			if (!coords) {
+				console.error("no coords found");
+				return;
+			}
 
 			const libMap = new maplibregl.Map({
 				container: mapContainerRef.current,
 				style: `https://api.maptiler.com/maps/dataviz-dark/style.json?key=${env.NEXT_PUBLIC_MAP_KEY}`,
 				center: [coords.lon, coords.lat],
-				zoom: 13,
+				zoom: 10,
 				canvasContextAttributes: {
 					antialias: true,
 				},
 			});
 
-			libMap.on("load", () => handleMapLoad(rainviewer));
+			libMap.on("load", async () => {
+				setLoading(false);
+				await handleMapLoad();
+			});
 
 			mapRef.current = libMap;
 		})();
@@ -97,11 +108,13 @@ export function WeatherMap() {
 				clearInterval(intervalRef.current);
 			}
 		};
-	}, []);
+	}, [meeting]);
 
 	return (
 		<div className="relative h-full w-full">
 			<div ref={mapContainerRef} className="absolute h-full w-full" />
+
+			{loading && <div className="h-full w-full animate-pulse rounded-lg bg-zinc-800" />}
 		</div>
 	);
 }
