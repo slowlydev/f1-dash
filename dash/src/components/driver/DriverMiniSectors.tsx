0be3fd142f3b187ad33e8cd89d697c936e6bf214
Slowlydev <me@slowly.dev>
2025-04-15 23:44:50 +0200

    styles: better yellow color (amber-400)

--- dash/src/components/driver/DriverMiniSectors.tsx
+++ dash/src/components/driver/DriverMiniSectors.tsx
@@ -33,7 +33,7 @@ export default function DriverMiniSectors({ sectors = [], bestSectors, tla }: Pr
 					<div className={clsx("flex", showMiniSectors ? "items-center gap-1" : "flex-col")}>
 						<p
 							className={clsx(
-								"text-lg font-semibold leading-none",
+								"text-lg leading-none font-semibold",
 								getTimeColor(sector.overallFastest, sector.personalFastest),
 								!sector.value ? "text-zinc-600" : "",
 							)}
@@ -42,7 +42,7 @@ export default function DriverMiniSectors({ sectors = [], bestSectors, tla }: Pr
 						</p>
 
 						{showBestSectors && (
-							<p className="text-sm font-medium leading-none text-zinc-600">
+							<p className="text-sm leading-none font-medium text-zinc-600">
 								{bestSectors && bestSectors[i].value ? bestSectors[i].value : "-- ---"}
 							</p>
 						)}
@@ -58,7 +58,7 @@ function MiniSector({ status, wide }: { status: number; wide: boolean }) {
 		<div
 			style={wide ? { width: 10, height: 5, borderRadius: 2 } : { height: 10, width: 8, borderRadius: 3.2 }}
 			className={clsx({
-				"bg-yellow-500": status === 2048 || status === 2052, // TODO unsure
+				"bg-amber-400": status === 2048 || status === 2052, // TODO unsure
 				"bg-emerald-500": status === 2049,
 				"bg-violet-600": status === 2051,
 				"bg-blue-500": status === 2064,
