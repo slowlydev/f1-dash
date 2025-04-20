6dcd8a0ba97e283215635abe063116e3f9739372
Slowlydev <me@slowly.dev>
2025-04-20 23:20:27 +0200

    refactor(live): audio endpoint

--- crates/live/src/server/audio.rs
+++ crates/live/src/server/audio.rs
@@ -1,30 +1,29 @@
-use axum::{extract::Query, response::IntoResponse};
+use axum::{extract::Query, http::StatusCode, response::IntoResponse};
 use serde::Deserialize;
+use std::env;
 use tracing::error;
 
 #[derive(Deserialize)]
-
 pub struct Params {
-    path: String
+    path: String,
 }
 
-pub async fn get_audio(Query(params): Query<Params>) -> Result<impl IntoResponse, axum::http::StatusCode> {
-    let Ok(_) = std::env::var("ENABLE_AUDIO_FETCH") else { return Err(axum::http::StatusCode::NOT_IMPLEMENTED) };
+pub async fn get_audio(Query(params): Query<Params>) -> Result<impl IntoResponse, StatusCode> {
+    let Ok(_) = env::var("ENABLE_AUDIO_FETCH") else {
+        return Err(StatusCode::NOT_IMPLEMENTED);
+    };
 
     let audio_url = format!("https://livetiming.formula1.com/static/{}", params.path);
-    match reqwest::get(&audio_url).await {
-        Ok(response) => {
-            match response.bytes().await {
-                Ok(bytes) => Ok(bytes.as_ref().to_vec()),
-                Err(_) => {
-                    error!("Failed to decode response from {}", audio_url);
-                    Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR)
-                }
-            }
-        }
-        Err(_) => {
-            error!("Failed to retrieve audio data from {}", audio_url);
-            Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR)
-        }
-    }
+
+    let Ok(response) = reqwest::get(&audio_url).await else {
+        error!("Failed to retrieve audio data from {}", audio_url);
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    };
+
+    let Ok(bytes) = response.bytes().await else {
+        error!("Failed to decode response from {}", audio_url);
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    };
+
+    Ok(bytes.as_ref().to_vec())
 }
