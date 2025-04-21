ada6136e83309346ba06a844fb48d3d024c69fad
Slowlydev <me@slowly.dev>
2025-04-21 12:07:02 +0200

    refactor(weather): imporve mobile ui

--- dash/src/app/dashboard/weather/map.tsx
+++ dash/src/app/dashboard/weather/map.tsx
@@ -108,7 +108,7 @@ export function WeatherMap() {
 			<div ref={mapContainerRef} className="absolute h-full w-full" />
 
 			{!loading && frames.length > 0 && (
-				<div className="absolute bottom-0 left-0 z-20 m-2 flex w-lg gap-4 rounded-lg bg-black/80 p-4 backdrop-blur-xs">
+				<div className="absolute right-0 bottom-0 left-0 z-20 m-2 flex gap-4 rounded-lg bg-black/80 p-4 backdrop-blur-xs md:right-auto md:w-lg">
 					<PlayControls playing={playing} onClick={() => setPlaying((v) => !v)} />
 
 					<Timeline frames={frames} setFrame={setFrame} playing={playing} setPlaying={setPlaying} />
