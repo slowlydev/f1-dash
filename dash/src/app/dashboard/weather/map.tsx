67607925a79a628d5644c359f60c606c4cdbdb62
Slowlydev <me@slowly.dev>
2025-05-26 22:27:59 +0200

    fix: map avoid maptiler

--- dash/src/app/dashboard/weather/map.tsx
+++ dash/src/app/dashboard/weather/map.tsx
@@ -74,7 +74,7 @@ export function WeatherMap() {
 
 			const libMap = new maplibregl.Map({
 				container: mapContainerRef.current,
-				style: `https://api.maptiler.com/maps/dataviz-dark/style.json?key=${env.NEXT_PUBLIC_MAP_KEY}`,
+				style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
 				center: coords ? [coords.lon, coords.lat] : undefined,
 				zoom: 10,
 				canvasContextAttributes: {
